fb2cdc7bb34f31c9d6241dd02c2f04a04a18860d
Juan Jose Olvera <juanjo.olvera84@outlook.com>
2025-03-08 00:29:00 -0600

    Changed Name of the sidebar

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from features import display_key_features
 st.markdown('<h1 style="display: flex; align-items: center;"><i class="fa-solid fa-film" style="margin-right: 10px;"></i> Kino Manager App</h1>', unsafe_allow_html=True)
 st.markdown('<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">', unsafe_allow_html=True)
 
-st.sidebar.title("Navegation")
+st.sidebar.title("Authentication")
 # Authenticate the user
 if authenticate_user():
     # If authenticated, display the features
